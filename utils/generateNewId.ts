a47bb9c7e31d4750fdcdee6659e39e6b4c953755
svetlana_korneeva <svet-ko@list.ru>
2023-10-28 22:01:11 +0300

    fix: all databases works with same generateId func

--- utils/generateNewId.ts
+++
@@ -1,8 +0,0 @@
-// Define the generateNewId function in a separate TypeScript file
-import { Category } from "../types/categories.js";
-
-export function generateNewId(categories: Category[]): number {
-  const ids: number[] = categories.map((category) => category.id);
-  const maxId: number = Math.max(...ids);
-  return maxId + 1;
-}
