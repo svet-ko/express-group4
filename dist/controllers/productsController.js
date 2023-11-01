b9a52f955ae31b44e5200b534db5feba5854d512
svetlana_korneeva <svet-ko@list.ru>
2023-11-01 15:27:34 +0200

    fix: after rebase

--- dist/controllers/productsController.js
+++ dist/controllers/productsController.js
@@ -2,31 +2,31 @@ import ProductsService from "../services/productsService.js";
 import { ApiError } from "../errors/ApiError.js";
 export function findAllProduct(_, res, next) {
     const products = ProductsService.findAll();
-    if (products.length === 0) {
-        next(ApiError.resourceNotFound("Products can't be fetched"));
-        return;
-    }
     res.json({ products });
 }
 export function findOneProduct(req, res, next) {
     const productId = Number(req.params.productId);
     const product = ProductsService.findOne(productId);
     if (!product) {
-        next(ApiError.resourceNotFound("Product not found."));
+        next(ApiError.resourceNotFound("Product not found"));
         return;
     }
     res.json({ product });
 }
-export function createOneProduct(req, res) {
+export function createOneProduct(req, res, next) {
     const newProduct = req.body;
     const product = ProductsService.createOne(newProduct);
+    if (!product) {
+        next(ApiError.internal("Product was not created. Please, try again"));
+        return;
+    }
     res.status(201).json({ product });
 }
 export function deleteOneProduct(req, res, next) {
     const productId = Number(req.params.productId);
     const product = ProductsService.deleteOne(productId);
     if (!product) {
-        next(ApiError.resourceNotFound("Product not found."));
+        next(ApiError.resourceNotFound("Product not found"));
         return;
     }
     res.json({ product });
@@ -36,10 +36,14 @@ export function updateOneProduct(req, res, next) {
     const updatesForProduct = req.body;
     const productToUpdate = ProductsService.findOne(productId);
     if (!productToUpdate) {
-        next(ApiError.resourceNotFound("Product not found."));
+        next(ApiError.resourceNotFound("Product not found"));
         return;
     }
     const updatedProduct = ProductsService.updateOne(productId, updatesForProduct);
+    if (!updatedProduct) {
+        next(ApiError.internal("Could not update product"));
+        return;
+    }
     res.json({ updatedProduct });
 }
 export default {
@@ -47,5 +51,5 @@ export default {
     findAllProduct,
     createOneProduct,
     deleteOneProduct,
-    updateOneProduct
+    updateOneProduct,
 };
