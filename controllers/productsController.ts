d96be0002bf1045293ea9e415b0463c6f8524379
svetlana_korneeva <svet-ko@list.ru>
2023-11-01 14:19:37 +0200

    fixed controllers error messages declaration

--- controllers/productsController.ts
+++ controllers/productsController.ts
@@ -1,12 +1,12 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 
-import ProductsService from "../services/productsService.js"
-import { ApiError } from "../errors/ApiError.js"
+import ProductsService from "../services/productsService.js";
+import { ApiError } from "../errors/ApiError.js";
 
 export function findAllProduct(_: Request, res: Response, next: NextFunction) {
   const products = ProductsService.findAll();
 
-  res.json({ products })
+  res.json({ products });
 }
 
 export function findOneProduct(
@@ -14,22 +14,31 @@ export function findOneProduct(
   res: Response,
   next: NextFunction
 ) {
-  const productId = Number(req.params.productId)
-  const product = ProductsService.findOne(productId)
+  const productId = Number(req.params.productId);
+  const product = ProductsService.findOne(productId);
 
   if (!product) {
-    next(ApiError.resourceNotFound("Product not found."))
-    return
+    next(ApiError.resourceNotFound("Product not found"));
+    return;
   }
 
-  res.json({ product })
+  res.json({ product });
 }
 
-export function createOneProduct(req: Request, res: Response) {
+export function createOneProduct(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const newProduct = req.body;
-  const product = ProductsService.createOne(newProduct)
+  const product = ProductsService.createOne(newProduct);
 
-  res.status(201).json({ product })
+  if (!product) {
+    next(ApiError.internal("Product was not created. Please, try again"));
+    return;
+  }
+
+  res.status(201).json({ product });
 }
 
 export function deleteOneProduct(
@@ -37,15 +46,15 @@ export function deleteOneProduct(
   res: Response,
   next: NextFunction
 ) {
-  const productId = Number(req.params.productId)
-  const product = ProductsService.deleteOne(productId)
+  const productId = Number(req.params.productId);
+  const product = ProductsService.deleteOne(productId);
 
   if (!product) {
-    next(ApiError.resourceNotFound("Product not found."))
-    return
+    next(ApiError.resourceNotFound("Product not found"));
+    return;
   }
 
-  res.json({ product })
+  res.json({ product });
 }
 
 export function updateOneProduct(
@@ -53,18 +62,21 @@ export function updateOneProduct(
   res: Response,
   next: NextFunction
 ) {
-  const productId = Number(req.params.productId)
+  const productId = Number(req.params.productId);
   const updatesForProduct = req.body;
   const productToUpdate = ProductsService.findOne(productId);
 
   if (!productToUpdate) {
-    next(ApiError.resourceNotFound("Product not found."))
-    return
+    next(ApiError.resourceNotFound("Product not found"));
+    return;
   }
 
-  const updatedProduct = ProductsService.updateOne(productId, updatesForProduct)
+  const updatedProduct = ProductsService.updateOne(
+    productId,
+    updatesForProduct
+  );
 
-  res.json({ updatedProduct })
+  res.json({ updatedProduct });
 }
 
 export default {
@@ -72,5 +84,5 @@ export default {
   findAllProduct,
   createOneProduct,
   deleteOneProduct,
-  updateOneProduct
-}
+  updateOneProduct,
+};
